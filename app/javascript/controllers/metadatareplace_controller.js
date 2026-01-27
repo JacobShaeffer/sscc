b2f884ebc7ca85077b1fc881b6c8ea8794065b95
Jacob Shaeffer <shaeffer.jacob@gmail.com>
2026-01-27 16:53:18 -0700

    renaming metadatum-replace to metadatareplace

---
+++ app/javascript/controllers/metadatareplace_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus';
+import { get } from "@rails/request.js"
+
+export const string_identifier = 'metadatareplace';
+
+export default class extends Controller {
+    static targets = ["replaceWithId"];
+    static values = {
+        url: String
+    }
+
+    replace(event) {
+        event.preventDefault();
+        console.log("metadatumReplace#replace", );
+        console.log("replace_with_id: ", this.replaceWithIdTarget.value);
+        console.log(this.urlValue);
+
+        let params = new URLSearchParams({
+            replace_with: this.replaceWithIdTarget.value,
+        });
+
+        get(`${this.urlValue}?${params}`, {
+            responseKind: "turbo-stream",
+        });
+
+    }
+}
